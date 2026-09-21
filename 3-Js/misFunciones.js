9062463602944ac2c23ff5fffcc1d1095e830c1c
josefinafalco7 <2527181@ucc.edu.ar>
2026-09-21 08:37:37 -0300

    Ejercicio grados_radianes

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -29,4 +29,16 @@ if(isNaN(valor)){
         document.getElementById("metro").value = valor * 1.09361;
     }
 }
+    function convertirGR(id) {
+        let grad, rad;
+        if(id == "grados") {
+            grad = document.getElementById("grados").value;
+            rad = grad * Math.PI / 180;
+        } else if(id == "radianes") {
+            rad = document.getElementById("radianes").value;
+            grad = rad * 180 / Math.PI;
+        }
+        document.getElementById("grados").value = grad;
+        document.getElementById("radianes").value = rad;
+    }
 
